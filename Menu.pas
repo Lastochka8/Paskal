38f433141a78d0f4a0781378d09ca0b293e6a2f0
Lastochka8 <147799807+Lastochka8@users.noreply.github.com>
2024-04-02 13:51:13 +0300

    Add files via upload

--- Menu.pas
+++ Menu.pas
@@ -1,43 +1,54 @@
 ﻿Program primer;
-var
-  n: integer; //не удалять!!! нужна для работы меню
+var number, sum: Integer;
+ //не удалять!!! нужна для работы меню
   // раздел для объявления переменных. 
-Begin
+Begin sum := 0;
         // не УДАЛЯТЬ!! Организовано меню
-     writeln ('введите номер задачи');
-     Readln(n);
-case n of
+ 
 
 //вычисление функции вариант 1
 1:
-  Begin 
+Begin
+writeln('Введите числа, 0 останавливает ввод');
+readln(number);
+writeln('Введи числj'); 
+readln(number);
+ 
+sum := sum + number;
+
+until number = 0;
+
+writeln('Сумма: ', sum);
+ end.
+
+  
     // сюда пишем код программы варианта
   end;
 
 //вычисление функции вариант 2
-2:
-  Begin 
+2:var
+  a, b, c: integer;
+begin
+  writeln('Введите 3 числа: ');
+  readln(a, b, c);
+  
+  if (a * a + b * b = c * c) or (a * a + c * c = b * b) or (b * b + c * c = a * a) then
+    writeln('Не катеты')
+  else
+    writeln('Катеты');
+ 
     // сюда пишем код программы варианта
   end;
   //вычисление функции вариант 3
 3:
-  Begin 
+  var a, b, c, d, m, n: integer; result,resultq: Real;
+
+begin writeln('Введи 4 числа :'); readln(a, b, c, d);
+
+result := (a + (b * c - d));
+resultq :=  (d - a * a);
+
+writeln('Ответ: ', result,'/',resultq);
+ 
     // сюда пишем код программы варианта
   end;
-  //вычисление функции вариант 4
-4:
-  Begin 
-    // сюда пишем код программы варианта
-  end;
-  //вычисление функции вариант 5
-5:
-  Begin 
-    // сюда пишем код программы варианта
-  end;
-//вычисление функции вариант 1
-6:
-  Begin 
-    // сюда пишем код программы варианта
-  end;
-  end;
-  end.
